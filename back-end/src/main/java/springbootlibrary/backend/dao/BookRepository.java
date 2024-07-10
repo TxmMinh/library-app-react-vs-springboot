5cfffb3666b26e8bd9c5d71cb61a5c7e988b0f2d
minhtx <minhtx@sparkminds.net>
2024-07-10 21:50:16 +0700

    create: fe checkout page

--- back-end/src/main/java/springbootlibrary/backend/dao/BookRepository.java
+++ back-end/src/main/java/springbootlibrary/backend/dao/BookRepository.java
@@ -8,6 +8,6 @@ import springbootlibrary.backend.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
-    
+
     Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
